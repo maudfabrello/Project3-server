6e054787edad18e7365918d1dc281b3519f4c4cf
Cl Sayart <clsayart@gmail.com>
2021-03-26 12:08:56 +0100

    routes create edit

--- routes/history.js
+++ routes/history.js
@@ -2,24 +2,30 @@ const express = require("express");
 const router = express.Router();
 const History = require("../models/History");
 
-router.patch("/api/history/:id", (req, res, next) => {
-    console.log(req.params);
-    console.log(req.body);
+//onclick function axios to this route
+router.post("/:id", (req, res, next) => {
   
-    //date getdate()
-        //buyer patch.user
-        //artwork req parmas id
-        //let user = req.session.currentUser._id;
-    
-   
-        History.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then((updatedDocument) => {
-          res.status(200).json(updatedDocument);
-        })
-        .catch((error) => {
-          res.status(500).json(error);
-        });
-    
-  });
+  let date = getDate();
+ 
+ const newStreetArt = {
+    purchasedArt:req.params.id,
+    buyer: req.session.currentUser.username,
+    quantity:1,
+    date: date
+  }
+
+  // created: {
+ // type: Date,
+  //default: Date.now
+
+  History.create(newStreetArt)
+  .then((HDocument) => {
+    res.status(200).json(HDocument);
+  })
+  .catch((error) => {
+    res.status(500).json(error);
+  })
+
+});
 
 module.exports = router;
