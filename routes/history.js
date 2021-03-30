3b743a6cc3e79db4b69ac02152782368215d30a9
Cl Sayart <clsayart@gmail.com>
2021-03-30 12:33:57 +0200

    history

--- routes/history.js
+++ routes/history.js
@@ -2,14 +2,30 @@ const express = require("express");
 const router = express.Router();
 const History = require("../models/History");
 
-//onclick function axios to this route
+router.get("/my-history/purchasedArt", (req, res, next) => {
+  History.find({ buyer: req.session.currentUser._id })
+   .populate("purchasedArt")
+    .then((historyDocuments) => {
+      res.status(200).json(historyDocuments);
+    })
+    .catch(next);
+
+   
+});
+router.get("/my-history/createdArt", (req, res, next) => {
+  Art.find({ creator: req.session.currentUser._id })
+  .then((artworkDocuments) => {
+    res.status(200).json(artworkDocuments);
+  })
+  .catch(next);
+
+   
+});
+
+
+
 router.post("/onepiece/:id", /*middleware*/(req, res, next) => {
-  //email - 
-  //database va chercher les artworks avec l'id du creator = user on line
-  //et les history avec l'id buyer
-  //qd il appuie sur buy artwork buyer ça ajoute son ID au champs buyer ou crée un history
-  //display sur la gallery à modifier on enlève les trucs achetés? ou bien on réduit la quantité d'un
-  // let date = getDate();
+  
   let buyer = req.session.currentUser ;
   
   console.log("BIBIENLIGNE",buyer);
@@ -40,6 +56,5 @@ console.log(newHistory);
 
 });
 
-//router get if user === buyer 
 
 module.exports = router;
