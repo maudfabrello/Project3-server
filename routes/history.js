4c6832d3b6b5fc74c894f7d4227d58e4235ddbf0
Cl Sayart <clsayart@gmail.com>
2021-03-31 15:13:25 +0200

    history clean

--- routes/history.js
+++ routes/history.js
@@ -5,60 +5,51 @@ const Art = require("../models/Art");
 
 router.get("/myhistory/purchasedArt", (req, res, next) => {
   History.find({ buyer: req.session.currentUser })
-   .populate("purchasedArt")
+    .populate("purchasedArt")
     .then((historyDocuments) => {
       res.status(200).json(historyDocuments);
     })
     .catch(next);
-
-   
 });
 
 router.get("/myhistory/createdArt", (req, res, next) => {
   let creator = req.session.currentUser;
-  console.log("BIBI",creator);
+  console.log("BIBI", creator);
   Art.find({ creator: creator.toString() })
-  .then((artworkDocuments) => {
-    res.status(200).json(artworkDocuments);
-  })
-  .catch(next);
+    .then((artworkDocuments) => {
+      res.status(200).json(artworkDocuments);
+    })
+    .catch(next);
+});
 
+router.post(
+  "/onepiece/:id",
+  /*middleware*/ (req, res, next) => {
+    let buyer = req.session.currentUser;
+
+    console.log("BIBIENLIGNE", buyer);
+    console.log(req.params);
+    let purchasedArt = req.params.id;
+    let newDate = new Date();
    
-});
 
+    const newHistory = {
+      purchasedArt: purchasedArt,
+      buyer: buyer,
+      
+      date: newDate,
+    };
 
-
-router.post("/onepiece/:id", /*middleware*/(req, res, next) => {
-  
-  let buyer = req.session.currentUser ;
-  
-  console.log("BIBIENLIGNE",buyer);
-  console.log(req.params)
-  let purchasedArt= req.params.id
-  let newDate = new Date();
-  // let quantity=1;
- 
- const newHistory = {
-    purchasedArt:purchasedArt,
-    buyer: buyer,
-    // quantity:quantity,
-    date: newDate
+    
+    console.log(newHistory);
+    History.create(newHistory)
+      .then((HDocument) => {
+        res.status(200).json(HDocument);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
   }
-//WHEN THEY CLICK BUY? WHEN THEY CLICK CREATE? TWO ROUTES? COMPONENT TO DISPLAY CREATED ARTWORK?
-//HOW TO MAKE A DELETE ON A SINGLE PAGE? DISPLAY COMPONENT WHEN WRITING THE ROUTE?
-  // created: 
- // type: Date,
-  //default: Date.now
-console.log(newHistory);
-  History.create(newHistory)
-  .then((HDocument) => {
-    res.status(200).json(HDocument);
-  })
-  .catch((error) => {
-    res.status(500).json(error);
-  })
-
-});
-
+);
 
 module.exports = router;
